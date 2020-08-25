f036fed27d8fb525b39a570a79bf578f426fd8cb
John Maitz <john.maitz@gmail.com>
2020-08-25 14:51:54 -0400

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -72,6 +72,13 @@ exports.save = function (req, res) {
  */
 exports.execute = function (req, res) {
 
+    console.log("5");	
+    console.log("4");	
+    console.log("3");	
+    console.log("2");	
+    console.log("1");	
+    console.log("Executed: "+req.body.inArguments[0]);
+    
     var requestBody = req.body.inArguments[0];
 
     const accountSid = requestBody.accountSid;
