9f6d228617098409b688662f5b75e2d3c0862afe
rar4u2 <rar4u2@gmail.com>
2021-08-04 19:55:40 +0500

    demo project
    Update for my project

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -100,7 +100,7 @@ define([
             "authToken": authToken,
             "messagingService": messagingService,
             "body": body,
-            "to": "{{Contact.Attribute.TwilioV1.TwilioNumber}}" //<----This should map to your data extension name and phone number column
+            "to": "{{Contact.Attribute.Contacts_with_phone.+16786733382}}" //<----This should map to your data extension name and phone number column
         }];
 
         payload['metaData'].isConfigured = true;
