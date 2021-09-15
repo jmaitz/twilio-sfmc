0c6771f5cddde5b550e7ae5de0f8af8f291f29b9
wjchoi <wonjeung.choi@amorepacific.com>
2021-09-15 16:22:05 +0900

    11th

--- routes/activity.js
+++ routes/activity.js
@@ -121,26 +121,26 @@ exports.execute = function (req, res) {
     res.send(200, 'Publish');
 
     // Used to decode JWT
-    // JWT(req.body, process.env.jwtSecret, (err, decoded) => {
+    JWT(req.body, process.env.jwtSecret, (err, decoded) => {
 
-    //     // verification error -> unauthorized request
-    //     if (err) {
-    //         console.error(err);
-    //         return res.status(401).end();
-    //     }
+        // verification error -> unauthorized request
+        if (err) {
+            console.error(err);
+            return res.status(401).end();
+        }
 
-    //     if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
+        if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
             
-    //         // decoded in arguments
-    //         var decodedArgs = decoded.inArguments[0];
+            // decoded in arguments
+            var decodedArgs = decoded.inArguments[0];
             
-    //         logData(req);
-    //         res.send(200, 'Execute');
-    //     } else {
-    //         console.error('inArguments invalid.');
-    //         return res.status(400).end();
-    //     }
-    // });
+            logData(req);
+            res.send(200, 'Execute');
+        } else {
+            console.error('inArguments invalid.');
+            return res.status(400).end();
+        }
+    });
 };
 
 
