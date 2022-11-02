0f597d2986332abb020583a573c8c792ab2c2304
ruze <ruze00@gmail.com>
2022-11-02 13:35:03 -0400

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -100,7 +100,7 @@ define([
             "authToken": authToken,
             "messagingService": messagingService,
             "body": body,
-            "to": "{{Contact.Attribute.APPT_EVENT_TRIGGER.RecipientMobile}}" //<----This should map to your data extension name and phone number column
+            "to": "{{Contact.Attribute.appointmentEvents.RecipientMobile}}" //<----This should map to your data extension name and phone number column
         }];
 
         payload['metaData'].isConfigured = true;
