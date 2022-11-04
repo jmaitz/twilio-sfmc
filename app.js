a57bb46998141e3828db9f756645cfe5e78fafb1
ruze00 <ruze.richards@zeel.com>
2022-11-04 16:46:16 -0400

    update link

--- app.js
+++ app.js
@@ -38,6 +38,11 @@ app.post('/journeybuilder/validate/', activity.validate );
 app.post('/journeybuilder/publish/', activity.publish );
 app.post('/journeybuilder/execute/', activity.execute );
 
+app.post('/save/', activity.save );
+app.post('/validate/', activity.validate );
+app.post('/publish/', activity.publish );
+app.post('/execute/', activity.execute );
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
