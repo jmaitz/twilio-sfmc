db549fe77b87bfab8dcc47f6eb6410f18d876dee
ruze00 <ruze.richards@zeel.com>
2022-11-04 17:23:11 -0400

    final

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -100,7 +100,7 @@ define([
             "authToken": authToken,
             "messagingService": messagingService,
             "body": body,
-            "to": "{{Contact.Attribute.appointmentEvents.RecipientMobile}}" //<----This should map to your data extension name and phone number column
+            "to": "{{Contact.Attribute.appointmentEvents.recipientMobile}}" //<----This should map to your data extension name and phone number column
         }];
 
         payload['metaData'].isConfigured = true;
