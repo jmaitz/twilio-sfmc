294eb141bf4754d7376f75223dd8db532fb89c28
David Ball <david.ball@zeel.com>
2022-11-07 12:31:03 -0800

    console.logs

--- routes/activity.js
+++ routes/activity.js
@@ -141,7 +141,11 @@ exports.execute = function (req, res) {
 
 
 exports.execute2 = function (req, res) {
-    console.log('ARRIVED IN EXECUTE2 proving SMC knows about this')
+    console.log('ARRIVED IN EXECUTE2 proving SMC knows about this');
+    console.log('whole req');
+    console.log(req);
+    console.log('req.body');
+    console.log(req.body);
     console.log("Executed: "+req.body.inArguments[0]);
 
     var requestBody = req.body.inArguments[0];
